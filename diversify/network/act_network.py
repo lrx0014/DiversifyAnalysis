bc22ed9e65a24f00bb97ebc66558d8e2e98d4f96
Ryan <lrx0014@hotmail.com>
2024-11-20 19:24:23 +0100

    update

--- diversify/network/act_network.py
+++ diversify/network/act_network.py
@@ -11,8 +11,8 @@ var_size = {
         'fc_size': 32*44
     },
     'SpeechCommand': {
-        'in_size': 160,
-        'ker_size': 5,
+        'in_size': 1,
+        'ker_size': 6,
         'fc_size': 160*32
     }
 }
