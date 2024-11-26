4bdd05263e321294a77ebaf2abd98573437eef5d
Ryan <lrx0014@hotmail.com>
2024-11-26 15:13:28 +0100

    impl: speech command dataset

--- diversify/utils/util.py
+++ diversify/utils/util.py
@@ -96,7 +96,7 @@ def act_param_init(args):
     # people group
     args.act_people = {
         'emg': [[i*9+j for j in range(9)]for i in range(4)],
-        'SpeechCommand': [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]],
+        'SpeechCommand': [[0],[1]],
         }
     tmp = {
         'emg': ((8, 1, 200), 6, 10),
