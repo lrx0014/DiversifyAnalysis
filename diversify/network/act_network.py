da3e644eaf2e7c4aeb25c761f6c8ea10e65adf12
Ryan <lrx0014@hotmail.com>
2024-11-26 16:41:22 +0100

    impl: speech command dataset, add some logs

--- diversify/network/act_network.py
+++ diversify/network/act_network.py
@@ -40,5 +40,7 @@ class ActNetwork(nn.Module):
 
     def forward(self, x):
         x = self.conv2(self.conv1(x))
+        print(f"====> x.shape {x.shape}")
         x = x.view(-1, self.in_features)
+        print(f"after x.view ====> x.shape {x.shape}")
         return x
