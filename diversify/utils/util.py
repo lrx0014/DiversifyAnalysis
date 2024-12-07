612286b7e1a467de1e14f6f86a66e079667ec990
Ryan <lrx0014@hotmail.com>
2024-12-07 02:02:23 +0100

    adjust network settings

--- diversify/utils/util.py
+++ diversify/utils/util.py
@@ -100,7 +100,7 @@ def act_param_init(args):
         }
     tmp = {
         'emg': ((8, 1, 200), 6, 10),
-        'SpeechCommand': ((160, 1, 21), 10, None),
+        'SpeechCommand': ((161, 1, 21), 10, 10),
         }
     args.num_classes, args.input_shape, args.grid_size = tmp[args.dataset][1], tmp[args.dataset][0], tmp[args.dataset][2]
 
